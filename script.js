5259e9f10e45a2979dbf24f11bde1c5d2b139640
John-Decripito <734507@sandwellcollege.ac.uk>
2025-03-19 14:35:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
